25d89132649724a7e6e65dc452ab8fa562894bbd
Teo Georgiev <t.georgiev.g@gmail.com>
2023-09-24 09:52:19 +0300

    Edits to ex12 - handling errors for nb being 1, 0, or negative. Solution for ex13. Exercises not validated with Moulinette.

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -3,6 +3,10 @@ int	ft_iterative_factorial(int nb)
 	int	i;
 
 	i = 1;
+	if (nb == 1 || nb == 0)
+		return (1);
+	if (nb < 0)
+		return (0);
 	while (nb > 0)
 	{
 		i *= nb;
---
+++ ex13/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb);
+
+int	main(void)
+{
+	int	nb;
+
+	nb = 4;
+	printf("RESULT:\t%d\n", ft_recursive_factorial(nb));
+	return (0);
+}
