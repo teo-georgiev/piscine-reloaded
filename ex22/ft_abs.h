a8bbc44947c0878e767e540362e9b91a4c609f30
Teo Georgiev <t.georgiev.g@gmail.com>
2023-09-25 14:09:48 +0300

    Solution for ex22, ft_abs.h. Not validated with Moulinette.

---
+++ ex22/ft_abs.h
@@ -0,0 +1,4 @@
+#ifndef FT_ABS_H
+# define FT_ABS_H
+# define ABS(nb) (nb < 0) * -nb + (nb >= 0) * nb
+#endif
---
+++ tests/ex22/main.c
@@ -0,0 +1,11 @@
+#include "ft_abs.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int	nb;
+
+	nb = -238217682;
+	printf("RESULT:\t%d\n", ABS(nb));
+	return (0);
+}
