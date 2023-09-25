efdd85649fa662cf1613660c587695a13f6729ae
Teo Georgiev <t.georgiev.g@gmail.com>
2023-09-25 12:15:11 +0300

    Solution to ex18. Not validated with Moulinette.

---
+++ tests/ex18/ft_print_params.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
