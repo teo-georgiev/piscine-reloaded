ca13a1d1a672c01a06a52c26dd3cc573aeb129f4
Teo Georgiev <t.georgiev.g@gmail.com>
2023-09-25 15:32:34 +0300

    Solution for ex24, Makefile. Not validated with Moulinette.

---
+++ tests/ex24/includes/ft_header.h
@@ -0,0 +1,12 @@
+#ifndef FT_HEADER_H
+# define FT_HEADER_H
+
+# include <unist.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
+
+#endif 
